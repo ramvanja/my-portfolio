159e5cded0a2b9ca0a3df800da79bc0d8935b054
ramvanja <ramdhan.vanjara@publicisresources.com>
2024-10-08 10:11:08 +0530

    Initial commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,9 @@
+export const contactsData = {
+    email: 'vanjararamdhan@gmail.com',
+    phone: '+91 7069989488',
+    address: 'Surat, Gujarat, India',
+    github: 'https://github.com/vanjararamdhan',
+    linkedIn: 'https://www.linkedin.com/in/ramdhan-vanjara/',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
